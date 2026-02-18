6c369c3a0c5cbb4ec32a7bb4130e26f73a90f8af
dmitrylean <adam.lean.92@gmail.com>
2026-02-18 17:15:15 +0800

    gamestore add promo and footer

--- web/gamestore/script.js
+++ web/gamestore/script.js
@@ -2,3 +2,14 @@
 document.querySelector(".member-btn").addEventListener("click", () => {
   alert("Member Area Clicked");
 });
+
+// Accordion
+
+document.querySelectorAll(".accordion-header").forEach((button) => {
+  button.addEventListener("click", () => {
+    const content = button.nextElementSibling;
+
+    button.classList.toggle("active");
+    content.classList.toggle("open");
+  });
+});
